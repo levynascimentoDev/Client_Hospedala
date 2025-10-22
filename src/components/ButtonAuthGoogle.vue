d140016b607976005f07f4ba293fafb51b5ab8da
levynascimentoDev <ffzikaorg@gmail.com>
2025-10-22 10:41:45 -0300

    teste

--- src/components/ButtonAuthGoogle.vue
+++ src/components/ButtonAuthGoogle.vue
@@ -50,7 +50,7 @@
                     if (resp.ok) {
                         const value:googleAuthResponse = await resp.json()
                         console.log(value)
-                        window.location.href = value.redirect_uri
+                        // window.location.href = value.redirect_uri
                     } else {
                         this.isLoading = false;
                         this.$emit('load', false)
