e1580b0836185e7048965a251313608baa924066
J.Rafael <joaorafaelsilveiradesouzatv@gmail.com>
2026-03-13 12:51:48 -0300

    first commit

---
+++ src/pages/TemporarySession/SelectYourPlace.vue
@@ -0,0 +1,39 @@
+<script lang="ts"dk></script>
+
+<template>
+    <div id="title">
+    <h1>Selecione o que deseja anunciar</h1>
+    <p>Por favor, descreva melhor o espaço que deseja anunciar.</p>
+    </div>
+
+    <div id="opitions">
+        <div class="box">
+            <i class="bi bi-house-door-fill"></i>
+            <p>Casa</p>
+            <p>Casa independente com espaço próprio</p>
+        </div>
+        <div class="box">
+            <p>Apartamento</p>
+            <p>Unidade em edifício residencial</p>
+        </div>
+        <div class="box">
+            <p>Motel</p>
+            <p>Estadia curta</p>
+        </div>
+        <div class="box">
+            <p>Pousada</p>
+            <p>Hospedagem familiar e acolhedora</p>
+        </div>
+        <div class="box">
+            <p>Hotel</p>
+            <p>Estabelecimento hoteleiro</p>
+        </div>
+        <div class="box">
+            <p>Resort</p>
+            <p>Hotel grande com laser completo</p>
+        </div>
+    </div>
+    <button>Continuar</button>
+</template>
+
+<style> </style>
