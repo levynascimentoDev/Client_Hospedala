763c6634e27ee8c5a2a4921a1ed419080a7d0a7b
J.Rafael <joaorafaelsilveiradesouzatv@gmail.com>
2026-03-17 22:41:23 -0300

    Fiz a tela de selecao de tipo de hospedagem

--- src/router/auth.routes.ts
+++ src/router/auth.routes.ts
@@ -2,11 +2,13 @@ import { type RouteRecordRaw } from 'vue-router'
 import Login from '../pages/auth/Login.vue'
 import Register from '../pages/auth/Register.vue';
 import CheckoutEmail from '../pages/auth/CheckoutEmail.vue';
+import SelectYourPlace from '../pages/TemporarySession/SelectYourPlace.vue';
 
 const authRoutes: RouteRecordRaw[] = [
     { path:"/login", component: Login },
     { path:"/login/complete", component: Register },
     { path:"/login/verification", component: CheckoutEmail },
+    { path: "/select", component: SelectYourPlace},
 ]
 
 export default authRoutes; 
