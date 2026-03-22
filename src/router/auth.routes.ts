330c0184f0bcc367042ae65f3138592d2fbafcc0
J.Rafael <joaorafaelsilveiradesouzatv@gmail.com>
2026-03-22 17:33:51 -0300

    fiz tela seleção tipo de hospedagem e a parte lógica da página

--- src/router/auth.routes.ts
+++ src/router/auth.routes.ts
@@ -3,6 +3,7 @@ import Login from '../pages/auth/pages/Login.vue'
 import Register from '../pages/auth/pages/Register.vue';
 import CheckoutEmail from '../pages/auth/pages/CheckoutEmail.vue';
 
+
 const authRoutes: RouteRecordRaw[] = [
     { path:"/login", component: Login },
     { path:"/login/complete", component: Register },
