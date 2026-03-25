dd7d1dfea779afed0c8f48200db5bb0cb6b7995a
J.Rafael <joaorafaelsilveiradesouzatv@gmail.com>
2026-03-25 11:07:09 -0300

    criei página de seleção de tipo de espaço com seleção de cards e botão continuar

--- src/router/host.routes.ts
+++ src/router/host.routes.ts
@@ -1,11 +1,10 @@
 import type { RouteRecordRaw } from "vue-router";
 import SelectYourPlace from "../pages/TemporarySession/SelectYourPlace.vue";
+import PlaceType from "../pages/TemporarySession/PlaceType.vue";
 
 const hostRoutes: RouteRecordRaw[] = [
-    {
-        path: "/select-type-place",
-        component: SelectYourPlace
-    }
+        {path: "/select-type-place", component: SelectYourPlace},
+        {path: "/place-type", component: PlaceType},
 ]
 
 export default hostRoutes;
