722d3c18ece6d43964af3736e075b26984bdca1b
levynascimentoDev <ffzikaorg@gmail.com>
2026-06-28 03:44:50 -0300

    fix

--- src/components/ui/TopBar.vue
+++ src/components/ui/TopBar.vue
@@ -39,7 +39,7 @@
 </template>
 <script setup lang="ts">
 import Menu from './Menu.vue';
-import SearchBar from '../app/SearchBar/index.vue';
+import SearchBar from '../app/searchbar/index.vue';
 
 import { useUserStore } from '../../stores/users.ts';
 import { ref } from 'vue';
